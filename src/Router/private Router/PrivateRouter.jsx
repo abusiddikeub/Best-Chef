6990978cdc5531bd19bea13675773d0b4f7e6cbb
abudiddikeub <abusiddikeub1@gmail.comm>
2023-05-04 17:40:29 +0600

    twelve commit

--- src/Router/private Router/PrivateRouter.jsx
+++ src/Router/private Router/PrivateRouter.jsx
@@ -1,31 +1,27 @@
-import React, { useContext } from 'react';
-import { AuthContext } from '../AuthProvider';
-import { Navigate, useLocation } from 'react-router-dom';
-import { Spinner } from 'react-bootstrap';
+import React, { useContext } from "react";
+import { AuthContext } from "../AuthProvider";
+import { Navigate, useLocation } from "react-router-dom";
+import { Spinner } from "react-bootstrap";
 
-const PrivateRouter = ({children}) => {
-               const {user,loading} = useContext(AuthContext)
-               const location = useLocation();
-               console.log(location)
-              
-               if(loading){
-                              return (
-                                        <div className=''>
-                                                  <Spinner className='text-center' animation="border" role="status">
-                                             <span className="visually-hidden">Loading...</span>
-                                           </Spinner>
-                                        </div>
-                              )
-               }
+const PrivateRouter = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+  const location = useLocation();
+  console.log(location);
 
-               if(user){
-                              return children
-               }
-               return (
-                              <Navigate state={{from:location}} to='/login' replace>
-                                             
-                              </Navigate>
-               );
+  if (loading) {
+    return (
+      <div className="bg-info text-center ">
+        <Spinner className="text-center" animation="border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      </div>
+    );
+  }
+
+  if (user) {
+    return children;
+  }
+  return <Navigate state={{ from: location }} to="/login" replace></Navigate>;
 };
 
 export default PrivateRouter;
